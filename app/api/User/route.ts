671fd275fa65329edc114111e73ceefad5cc81e2
it21294198 <it21294198@my.sliit.lk>
2023-09-04 21:58:21 +0530

    edit api

--- app/api/User/route.ts
+++ app/api/User/route.ts
@@ -5,9 +5,9 @@ export async function POST(request:Request) {
 
   try{
 
-    const {email,password} = await request.json();
-    console.log(email);
-    console.log(password);
+    // const {email,password} = await request.json();
+    // console.log(email);
+    // console.log(password);
     console.log("Should run");
     return NextResponse.json({message:'Okeee'})
 
