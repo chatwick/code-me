52529de1d2bcd32db6b6b79c1f23f7f800c2a1be
it21294198 <it21294198@my.sliit.lk>
2023-09-04 22:21:07 +0530

    debug the api folder

--- app/api/User/route.ts
+++ app/api/User/route.ts
@@ -1,15 +1,42 @@
-// import db from '../../utils/mysqldb'
+
 import { NextResponse,NextRequest } from 'next/server'
 
+// this is get request
+// http://localhost:3000/api/user (For get request)
+export async function GET(request:Request) {
+  try{
+
+    console.log("GET request ran");
+    return NextResponse.json({message:'Successful GET Request'})
+
+  }catch(err){
+
+    return NextResponse.json({error:'Error !!!!!!!!!'})
+
+  }
+  
+}
+
+// this is post request (To open press CTRL + hover on the link)
+// http://localhost:3000/api/user
 export async function POST(request:Request) {
 
   try{
 
-    // const {email,password} = await request.json();
-    // console.log(email);
-    // console.log(password);
-    console.log("Should run");
-    return NextResponse.json({message:'Okeee'})
+    // paste the following on the API for JSON object section
+    // {"name":"Hello world","age":22}
+
+    //Following commented lines will get the data 
+    // from frontend and then log them on server console
+    const requestObject = await request.json()
+
+    // log hole object
+    console.log(requestObject);
+    // log part of a object
+    console.log(requestObject.name);
+
+    console.log("POST request ran");
+    return NextResponse.json({message:'Successful POST Request'})
 
   }catch(err){
 
