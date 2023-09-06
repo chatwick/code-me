a3d5738c4e74d77d889d111c35f9666f83ceb02d
it21294198 <it21294198@my.sliit.lk>
2023-09-06 21:37:19 +0530

    Bug fixed

--- app/components/Diagram.js
+++ app/components/Diagram.js
@@ -1,16 +1,19 @@
 'use client'
 import { useEffect,useState } from 'react';
 import DiagramFactory from '../CanvasDiagram/DiagramFactory'
-import FilterObjects from './FilterObjects'
 
 function Diagram() {
   
-  // import the Set of Objects
-  const [obj,setObj] = useState(FilterObjects('myStack'))
-  // const [obj,setObj] = useState(FilterObjects('newStack'))
-
+  const [FileContent, setFileContent] = useState(0);
+  const [FileContentType, setFileContentType] = useState(0);
+  const [File, setFile] = useState([]);
+  const [data, setData] = useState(null);
+  const [obj,setObj] = useState([])
   const [objCount, setCount] = useState(0);
   const [ctx, setCtx] = useState(null);
+
+  const pushOperations = [];
+  const operations = [];
 
   const updateContext = (obj) =>{
     console.log('The object : ',obj);
@@ -40,27 +43,96 @@ function Diagram() {
   console.log('Object in Diagram : ',obj);
 
   useEffect(() => {
-    
+    // ###############################################################################
+      const stackName = 'myStack' 
+      const pushRegex = new RegExp(`${stackName}\\.push\\((\\d+)\\)`, 'g');
+      const operationRegex = new RegExp(`(${stackName}\\.push|${stackName}\\.pop)\\(`, 'g');
+      
+      fetch('/code.java')
+      .then((response) => response.text())
+      .then((data) => {
+        // console.log(data);
+        setData(data)
+        // get stack data from the file
+        let match;
+        while ((match = pushRegex.exec(data)) !== null) {
+          pushOperations.push(parseInt(match[1]));
+        }
+        // get the type of the operation from the file
+        let match1;
+        while ((match1 = operationRegex.exec(data)) !== null) {
+          const operationType = match1[1] === `${stackName}.push` ? 1 : 0;
+          operations.push(operationType);
+        }
+        setFileContent(pushOperations)
+        setFileContentType(operations)
+        
+        let mainObj = []
+        let mainArr = 0 
+      
+        while(FileContent.length-1 >= mainArr){
+          
+          let subArr = mainArr
+        let subObj = []
+        let index = 0
+        
+        while(subArr >= index){
+          
+          let parentId = index - 1;
+          if(index === 0) parentId = null;
+          
+          let chileId = index + 1;
+          if(index === subArr) chileId = null;
+          
+          subObj.push({
+            "id":index,
+            "value":FileContent[index],
+            "parent_id":parentId,
+            "left_id":null,
+            "right_id":chileId
+          })
+          
+          index = index + 1;
+        }
+        
+        mainObj.push(subObj) 
+        mainArr = mainArr + 1
+        // console.log('Main index : ',mainArr,'Object : ',subObj);
+      }
+      
+      // console.log('Main object : ',mainObj);
+      console.log('Object in FilerObject : ',mainObj);
+      setFile(mainObj)
+      setObj(mainObj)
+      
+      // return mainObj
+      // }
+    })
+    .catch((error) => {
+      console.error('Error fetching the file:', error);
+    });
+
+    // ###############################################################################
     const canvas = document.getElementById("myCanvas");
     // Check if the canvas and 2D context are available.
     
     if (canvas) {
       const context = canvas.getContext("2d");
       // Perform your transformations and draw your diagram here.
-      context.translate(70, 70);
+      context.translate(0, 0);
       // Create a DiagramFactory and draw your diagram using the 'context' here.
       new DiagramFactory(context, 0, obj[objCount]);
       // Update the state with the 2D context.
       setCtx(context);
     }
-  },[]);
+  },[data]);
   
   return (
     <div>
       <button className='text-black p-20 bg-slate-300 m-5 hover:bg-slate-800 hover:text-white' onClick={handleVisualize}>Visualize</button>
-      <canvas id='myCanvas' width="1000" height="1000" className='border-solid border-spacing-x-1 bg-gray-600'></canvas>
       <button className='text-black p-20 bg-slate-300 m-5 hover:bg-slate-800 hover:text-white' onClick={handlePrevious}>Previous</button>
       <button className='text-black p-20 bg-slate-300 m-5 hover:bg-slate-800 hover:text-white' onClick={handleNext}>Next</button>
+      <canvas id='myCanvas' width="1000" height="1000" className='border-solid border-spacing-x-1 bg-gray-600'></canvas>
     </div>
   )
 }
---
+++ public/code.java
@@ -0,0 +1,19 @@
+// Stack myStack = new Stack();
+
+// myStack.push(17)
+// myStack.push(32)
+// myStack.push(64)
+// myStack.pop()
+// myStack.push(87)
+// myStack.push(7)
+// myStack.pop()
+
+// Stack newStack = new Stack()
+
+// newStack.push(12)
+// newStack.push(23)
+// newStack.pop()
+// newStack.push(34)
+// newStack.push(45)
+// newStack.pop()
+// newStack.push(56)
