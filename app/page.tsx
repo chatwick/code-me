760405040a8d171a61fb268265e2a73ebf2b5d12
it21292422 <it21292422@my.sliit.lk>
2023-09-07 21:25:36 +0530

    firebase config

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,39 @@
-import { randomUUID } from "crypto";
+"use client"
 import { MessageCard } from "./components/MessageCard";
 import addData from "@/firebase/firestore/addData";
+import { useState } from "react";
 
 export default function Home() {
 
-  async function sendMessage(data: FormData) {
-    "use server"
-    const message = data.get("msg")?.valueOf()
-    const { result, error } = await addData('discussion','M001', message)
+  const [message,setMessage] = useState("");
 
-    if (error) {
-      return console.log(error)
+  async function sendMessage(data: FormData) {
+    const msg = data.get("msg")?.valueOf()
+    if(!msg){
+      const error = document.getElementById('error') as HTMLFormElement
+      error.value = "Message cannot be empty"
     }
+    const msgData ={
+      msg,
+      user: "John Smith"
+    }
+    await addData(msgData)
+    setMessage("")
   }
 
   return (
-    <>
+    <div className="flex flex-col h-screen">
     <header className="flex justify-between items-center mb-4">
       <h1 className="text-2xl">Discussion Forum</h1>
     </header>
     <MessageCard/>
-    <form action={sendMessage}>
-    <textarea name="msg" className="textarea textarea-bordered textarea-lg w-full max-w-xs flex items-stretch"/>
+    <form className="flex-grow" action={sendMessage}>
+    <textarea name="msg" value={message} onChange={(e) => setMessage(e.target.value)} className="textarea textarea-bordered textarea-lg w-full max-w-xs flex items-stretch"/>
+    <label className="label">
+    <span id="error" className="label-text-alt text-red-600"></span>
+  </label>
     <button type="submit" className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">Send</button>
     </form>
-    </>
+    </div>
   )
 }
