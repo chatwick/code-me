befcf41a58b7b004c857e9dffd9dc97b28db3cb0
IT21294648 <c.wick400@gmail.com>
2023-09-07 17:04:09 +0530

    fixed some bugs

--- app/page.tsx
+++ app/page.tsx
@@ -29,13 +29,13 @@ export default function Home()
 {
   return (
     <main className="flex flex-col">
-      <div className="bg-customBG1 absolute h-[70vh] lg:h-[80vh] w-full object-cover object-right">
+      <div className="bg-customBG1 h-[70vh] lg:h-[80vh] w-full object-cover object-right">
 
         <h1 className='text-5xl text-white font-bold text-center mt-10'>Coding is hard</h1>
         <h2 className='text-5xl text-white font-bold text-center'>We know</h2>
 
         <div className='mx-40 px-60 my-20'>
-          <p>
+          <p className='text-slate-200'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
             exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
@@ -45,7 +45,24 @@ export default function Home()
         </div>
 
         <div className='flex justify-center'>
-          <Link className='btn btn-primary w-40' href="/editor/new-editor">Try it</Link>
+          <Link className='btn btn-primary w-40 mb-10' href="/editor/new-editor">Try it</Link>
+        </div>
+        <div className="flex items-center justify-center">
+          <div className="card card-side w-2/3 bg-base-100 shadow-xl">
+            <figure className='w-1/2'><Image
+              src={codeImage}
+              alt='codeSampleImage'
+              quality={100}
+              layout="responsive"
+            /></figure>
+            <div className="card-body">
+              <h2 className="card-title text-md ">An Editor built for beginners !</h2>
+              <p>Click the button to watch on Jetflix app.</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Watch</button>
+              </div>
+            </div>
+          </div>
         </div>
 
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        'customBG1': "url('../public/monacoSample2.jpg')",
+        'customBG1': "url('../public/monacoSample2Edited.jpg')",
         'hero-pattern': "linear-gradient(to right bottom, rgba('#7ed56f',0.8), rgba('#28b485',0.8)), url('../public/monacoSample2.jpg')",
       },
     },
