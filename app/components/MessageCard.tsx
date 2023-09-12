33d3e5b3b7db09b33c59ac57f09a2bea15d4e9dc
it21292422 <it21292422@my.sliit.lk>
2023-09-12 22:16:28 +0530

    done

--- app/components/MessageCard.tsx
+++ app/components/MessageCard.tsx
@@ -1,24 +1,32 @@
 import Image from 'next/image'
 import userPic from '../assets/user1.png'
+import { FC } from 'react'
 
-export function MessageCard() {
+type Message = {
+    message: string
+    user: string
+    userImage:string
+    dateTime: any
+}
+const MessageCard:FC <Message> = ({message,user,dateTime,userImage}):JSX.Element => {
     return (
         <>
             <div className="chat chat-start">
                 <div className="chat-image avatar">
                     <div className="w-10 rounded-full">
-                        <Image src={userPic} width={500} height={500} alt="user"/>
+                        <Image src={userImage} width={500} height={500} alt="user"/>
                     </div>
                 </div>
                 <div className="chat-header gap-1">
-                    John Smith &nbsp;
+                    {user} &nbsp;
                     <time className="text-xs opacity-50">12:45</time>
                 </div>
-                <div className="chat-bubble chat-bubble-primary">Hi, I would like to know how to use threads in Java</div>
+                <div className="chat-bubble chat-bubble-primary">{message}</div>
                 <div className="chat-footer opacity-50">
-                    Delivered
+                    {dateTime}
                 </div>
             </div>
         </>
     )
 }
+export default MessageCard
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 "use client"
-import { MessageCard } from "./components/MessageCard";
+import MessageCard  from "./components/MessageCard";
+//import { collection, getDocs} from "firebase/firestore";
 import addData from "@/firebase/firestore/addData";
-import { useState } from "react";
+import getData from "@/firebase/firestore/getData";
+import { useState, useEffect } from "react";
+import { firestore } from "@/firebase/config";
+import {collection,QueryDocumentSnapshot,DocumentData,query,where,limit,getDocs} from "@firebase/firestore";
+const discussionCollection = collection(firestore,'discussion');
+const db = firestore;
+
+type Message = {
+  message: string
+  user: string
+  dateTime: Date
+}
 
 export default function Home() {
 
@@ -11,27 +23,89 @@ export default function Home() {
     const msg = data.get("msg")?.valueOf()
     if(!msg){
       const error = document.getElementById('error') as HTMLFormElement
-      error.value = "Message cannot be empty"
+      error.innerHTML = "Message cannot be empty"
+    }else{
+      const error = document.getElementById('error') as HTMLFormElement
+      error.innerHTML = ""
+      const msgData ={
+        msg,
+        user: "John Smith"
+      }
+      await addData(msgData)
+      setMessage("")
     }
-    const msgData ={
-      msg,
-      user: "John Smith"
-    }
-    await addData(msgData)
-    setMessage("")
   }
 
+ 
+//   async function getMessages(){
+//     const querySnapshot = await getDocs(collection(db, "cities"));
+//     querySnapshot.forEach((doc) => {
+//     // doc.data() is never undefined for query doc snapshots
+//     console.log(doc.id, " => ", doc.data());
+// });
+//   }
+async function receiveData(){
+  console.log("This is receive data")
+  await getData()
+}
+receiveData()
+
+ const newMessage={
+    message:"Hi I am new to Java",
+    user:"John Smith",
+    userImage:"/user1.png",
+    dateTime:"24-12-2023"
+ }
+
+ const newMessage2={
+  message:"Can you help me to understand OOP Concepts",
+  user:"John Smith",
+  userImage:"/user1.png",
+  dateTime:"24-12-2023"
+}
+
+const newMessage3={
+  message:"I think I have difficulty in understanding Abstaction and Encapsulation",
+  user:"John Smith",
+  userImage:"/user1.png",
+  dateTime:"24-12-2023"
+}
+
+const newMessage4={
+  message:"It will be really helpful if some can share some good tutorials",
+  user:"John Smith",
+  userImage:"/user1.png",
+  dateTime:"24-12-2023"
+}
+
+const newMessage5={
+  message:"You can check this website `https://www.w3schools.com/java/java_oop.asp` it will explain in a simple way",
+  user:"Dave Gray",
+  userImage:"/user2.png",
+  dateTime:"24-12-2023"
+}
+const newMessage6={
+  message:"Free code camp also had a good video on this check that out as well",
+  user:"Dave Gray",
+  userImage:"/user2.png",
+  dateTime:"24-12-2023"
+}
   return (
     <div className="flex flex-col h-screen">
     <header className="flex justify-between items-center mb-4">
       <h1 className="text-2xl">Discussion Forum</h1>
     </header>
-    <MessageCard/>
+    <MessageCard {...newMessage}/>
+    <MessageCard {...newMessage2}/>
+    <MessageCard {...newMessage3}/>
+    <MessageCard {...newMessage4}/>
+    <MessageCard {...newMessage5}/>
+    <MessageCard {...newMessage6}/>
     <form className="flex-grow" action={sendMessage}>
     <textarea name="msg" value={message} onChange={(e) => setMessage(e.target.value)} className="textarea textarea-bordered textarea-lg w-full max-w-xs flex items-stretch"/>
     <label className="label">
     <span id="error" className="label-text-alt text-red-600"></span>
-  </label>
+    </label>
     <button type="submit" className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">Send</button>
     </form>
     </div>
