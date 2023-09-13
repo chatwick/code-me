a20a1c50b023d62c17d703b7ec74747b3e169136
it21292422 <it21292422@my.sliit.lk>
2023-09-13 09:18:55 +0530

    done firebase

--- firebase/config.js
+++ firebase/config.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAe1od_jy8uzBASofkWfxL5qQxnpKXh9gw",
-  authDomain: "code-me-601b0.firebaseapp.com",
-  projectId: "code-me-601b0",
-  storageBucket: "code-me-601b0.appspot.com",
-  messagingSenderId: "779657892073",
-  appId: "1:779657892073:web:4040cb0b1cf6ab3b68e078"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
