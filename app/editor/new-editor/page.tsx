ca8b40d6d46541c1d02281524907d91f0ede39b4
IT21294648 <c.wick400@gmail.com>
2023-09-15 22:24:59 +0530

    added divider between editor and visualizer

--- app/editor/new-editor/page.tsx
+++ app/editor/new-editor/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { App } from './NewEditor'
 import Output from './Output'
 import Diagram from '../../components/Diagram'
@@ -9,25 +9,29 @@ export default function EditorUI()
   const [data, setData] = useState('');
   const [view, setView] = useState(true);
 
-  useEffect(() => {
+  useEffect(() =>
+  {
     setData('newStack.push(12)newStack.push(23)newStack.pop()newStack.push(34)newStack.push(45)newStack.push(56)newStack.pop()newStack.pop()')
   }, [])
 
-  const handleview = () => {
-      const viewState = view===true ? false : true ;  
-      setView(viewState)
+  const handleview = () =>
+  {
+    const viewState = view === true ? false : true;
+    setView(viewState)
   }
-   
+
 
   return (
     <main>
-      <button className='btn btn-primary m-1' onClick={handleview}>{view ? 'Visualizer':'Output'}</button>
+      <button className='btn btn-primary m-1' onClick={handleview}>{view ? 'Visualizer' : 'Output'}</button>
       <div className="flex flex-row">
         <div className=" rounded-md lg:w-2/3 lg:h-full sm:w-full shadow-4xl ">
           <App />
         </div>
+        <div className="divider divider-horizontal"></div>
+
         <div className="flex flex-auto">
-          { view ? <Output/> : <Diagram code={data}/> }
+          {view ? <Output /> : <Diagram code={data} />}
         </div>
       </div>
     </main>
