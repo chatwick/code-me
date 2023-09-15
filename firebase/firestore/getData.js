e3b4fb0bb41a5fdced95ba66a06b23ed4a68364d
IT21292422 <it21292422@my.sliit.lk>
2023-09-15 22:59:31 +0530

    fixed order by when receiving data

--- firebase/firestore/getData.js
+++ firebase/firestore/getData.js
@@ -1,12 +1,13 @@
 import firebase_app from "../config";
-import { getFirestore, collection, doc, addDoc, updateDoc, serverTimestamp, getDocs } from "firebase/firestore";
+import { getFirestore, collection, doc, addDoc, updateDoc, serverTimestamp, getDocs, query, orderBy } from "firebase/firestore";
 import MessageCard from "@/app/components/MessageCard";
 
 const db = getFirestore(firebase_app)
 
 export default async function getData() {
 const messages = [];
-const querySnapshot = await getDocs(collection(db, "discussion"));
+const q = query(collection(db, "discussion"),orderBy("timestamp"))
+const querySnapshot = await getDocs(q);
 querySnapshot.forEach((doc) => {
   messages.push(doc.data())
   //console.log(doc.id, " => ", doc.data());
