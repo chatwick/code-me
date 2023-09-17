f67074f8e6ec3094fb48e6eb2a1d0273ed28d395
chamath-shanaka <chamathshanaka19@gmail.com>
2023-09-18 05:27:19 +0530

    NewErrorForm

---
+++ app/admin/page.tsx
@@ -0,0 +1,14 @@
+import NewErrorForm from "./NewErrorForm"
+
+export default function AdminUI() {
+
+  return (
+    <main>
+        <div className="overlay rounded-md w-full h-full shadow-4xl">
+					<NewErrorForm />
+        </div>
+    </main>
+
+
+  )
+}
