2fae57a8d3fba1ac3076edcf639a2c5e94bd8614
IT21292422 <it21292422@my.sliit.lk>
2023-09-17 15:00:21 +0530

    menu v2

--- app/components/MessageCard.tsx
+++ app/components/MessageCard.tsx
@@ -1,15 +1,18 @@
 import Image from 'next/image'
 import userPic from '../assets/user1.png'
-import { FC } from 'react'
+import { useState, FC } from 'react'
 
 type Message = {
+    //id: string
     message: string
     user: string
     userImage: string
     date: any
     time: any
 }
-const MessageCard: FC<Message> = ({ message, user, date, time, userImage }): JSX.Element => {
+
+const MessageCard: FC<Message> = ({message, user, date, time, userImage }): JSX.Element => {
+    const [data,setData] = useState("");
     return (
         <>
             <div className="chat chat-start relative">
@@ -21,17 +24,37 @@ const MessageCard: FC<Message> = ({ message, user, date, time, userImage }): JSX
                 <div className="chat-header gap-1">
                     {user} &nbsp;
                     <time className="text-xs opacity-50">{time}</time>
-                    <div className="dropdown dropdown-hover">
-                        <label tabIndex={0} className="link link-hover">&#xFE19;</label>
+                    <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className="btn btn-circle btn-ghost btn-xs text-info">
+                            &#xFE19;
+                        </label>
                         <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Edit</a></li>
+                            <li><button onClick={() => {
+                                const modal = document.getElementById('editModal') as HTMLDialogElement
+                                modal.showModal()
+                            }}>Edit</button></li>
+
+                            <dialog id="editModal" className="modal">
+                                <div className="modal-box">
+                                    <h3 className="font-bold text-lg">Update Message</h3>
+                                    <p className="py-4">Press ESC key or click the button below to close</p>
+                                    <textarea name="msg" value={data} onChange={(e) => setData(e.target.value)} className="textarea textarea-bordered textarea-lg w-full max-w-xs flex items-stretch"/>
+                                    <div className="modal-action">
+                                        <form method="dialog">
+                                            {/* if there is a button in form, it will close the modal */}
+                                            <button className="btn">Cancel</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </dialog>
+
                             <li><a>Delete</a></li>
                         </ul>
                     </div>
                 </div>
                 <div className="chat-bubble chat-bubble-primary">
 
-                {message}
+                    {message}
                 </div>
                 <div className="chat-footer opacity-50">
                     {date}
