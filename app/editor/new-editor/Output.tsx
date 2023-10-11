bfa0564b5a41140cf77d42010147a9b583c2a3d0
IT21294648 <c.wick400@gmail.com>
2023-10-12 01:29:41 +0530

    added terminal

--- app/editor/new-editor/Output.tsx
+++ app/editor/new-editor/Output.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Terminal } from 'xterm'
+import { FitAddon } from 'xterm-addon-fit';
 
-export default function Output() {
+export default function Output()
+{
+  let term = new Terminal();
+  const fitAddon = new FitAddon();
+  useEffect(() =>
+  {
+    const xtermTerminal = document.getElementById('terminal') as HTMLElement;
+    term.loadAddon(fitAddon);
+    term.open(document.getElementById('terminal') as HTMLElement );
+    fitAddon.fit();
+    term.write('Terminal started')
+  })
+
+  term.onKey((e)=> {
+    term.write(e.key)
+  })
+
+
   return (
     <main className='flex flex-1'>
 
       <div className="flex flex-col w-full">
         <h2 className='text-center'>Output</h2>
-        <small className='text-center'>Compiler not implemented yet</small>
+        <div id="terminal"></div>
       </div>
 
     </main>
